00ac08abf0faee16f4ec312488a132755ed31e01
Nabhan <jawahiirn@gmail.com>
2025-12-20 12:00:54 +0600

    basic controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get()
   findAll() {
     return 'YO COFFEES IN COFFEES';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This is #${id} NO. coffee found.`;
+  }
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body('name') name: string) {
+    return name;
   }
 }
