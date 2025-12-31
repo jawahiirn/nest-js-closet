f7e1ef8718e4c4055a4f3871c40f7522da75e166
Nabhan <jawahiirn@gmail.com>
2026-01-01 01:09:57 +0600

    coffees module: test for create coffee [POST] method.

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,11 +1,18 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import request from 'supertest';
+import { CreateCoffeeDto } from '../../src/coffees/dto/create-coffee.dto';
 
 describe('[Feature] Coffees - /coffees', () => {
   let app: INestApplication;
+  const coffee = {
+    name: 'Bottom Ocean Bloat',
+    brand: 'Buddy Brew',
+    flavors: ['creamy', 'chocolate', 'vanilla'],
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -28,10 +35,32 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
     await app.init();
   });
 
-  it.todo('Create [POST /]');
+  it('Create [POST /]', async () => {
+    const { body } = await request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED);
+    const expectedCoffee = expect.objectContaining({
+      ...coffee,
+      flavors: expect.arrayContaining(
+        coffee.flavors.map((name) => expect.objectContaining({ name })),
+      ),
+    });
+    expect(body).toEqual(expectedCoffee);
+  });
   it.todo('Get all [GET /]');
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PATCH /:id]');
