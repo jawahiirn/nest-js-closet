4d691a0274453059e90ad6bec35d8b70a2a5f10d
Nabhan <jawahiirn@gmail.com>
2026-01-03 23:27:38 +0600

    signUp service: created signUp route created signUp DTO

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
+import { HashingService } from '../hashing/hashing.service';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.userRepository.save(user);
+    } catch (error) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (error.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException();
+      }
+      throw error;
+    }
+  }
+}
