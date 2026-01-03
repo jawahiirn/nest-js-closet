5041cc497667c04db0a97ce7e504c206e4966e33
Nabhan <jawahiirn@gmail.com>
2026-01-03 23:21:39 +0600

    created: 1. authentication service 2. sign in+out dtos 3. authentication controller

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BcryptService } from './hashing/bcrypt.service';
 import { HashingService } from './hashing/hashing.service';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
 
 @Module({
   // Hashing Service is the resolved ? then point to BcryptService
   // When Hashing service is resolved the interface Bcrypt will be pointed to.
-  providers: [{ provide: HashingService, useClass: BcryptService }],
+  providers: [{ provide: HashingService, useClass: BcryptService }, AuthenticationService],
+  controllers: [AuthenticationController],
 })
 export class IamModule {}
