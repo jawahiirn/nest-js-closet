b43cbeb990153a8b9bf9cc18a0a9aa9635bae676
Nabhan <jawahiirn@gmail.com>
2026-01-06 00:25:23 +0600

    protecting routes with guard

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -8,12 +8,18 @@ import { User } from '../users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './guards/access-token.guard';
 
 @Module({
   // Hashing Service is the resolved ? then point to BcryptService
   // When Hashing service is resolved the interface Bcrypt will be pointed to.
   providers: [
     { provide: HashingService, useClass: BcryptService },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
     AuthenticationService,
   ],
   controllers: [AuthenticationController],
@@ -23,4 +29,4 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forFeature(jwtConfig),
   ],
 })
-export class IamModule {}
+export class IamModule { }
