30395a43567f48b375c577abe71f5b883e9fdab5
Nabhan <jawahiirn@gmail.com>
2026-01-13 23:03:03 +0600

    add 2FA columns to user entity and create update function for 2FA for user.

--- src/iam/authentication/otp-authentication.service.ts
+++ src/iam/authentication/otp-authentication.service.ts
@@ -3,12 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../users/entities/user.entity';
 import { generateSecret, generateURI, verify } from 'otplib';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OtpAuthenticationService {
   constructor(
     private readonly configService: ConfigService,
-    @InjectRepository(User) private readonly userRepository: User,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
   async generateSecret(email: string) {
@@ -23,5 +24,21 @@ export class OtpAuthenticationService {
 
   verify(token: string, secret: string) {
     return verify({ secret, token });
+  }
+
+  async enableTfaForUser(email: string, secret: string) {
+    const { id } = await this.userRepository.findOneOrFail({
+      where: { email },
+      select: { id: true },
+    });
+    await this.userRepository.update(
+      { id },
+      // TIP: Encrypt the secret instead of saving the string itself.
+      // Note: We could not hash, since original string is used for verification.
+      {
+        tfaSecret: secret,
+        isTfaEnabled: true,
+      },
+    );
   }
 }
